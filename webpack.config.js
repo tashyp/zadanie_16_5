4a0971e0b92df703c156c22d54d122172a7b108c
Przemek Saja <przemek.saja@gmail.com>
2018-07-25 17:32:34 +0200

    Add files with environment configuration

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = (env) => {
+    return {
+        mode: env || 'production',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+    }
+};
